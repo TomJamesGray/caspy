640f30361c55a38bbcb0f41136ad02bffd2f48a3
Tom Gray <tomg77@yahoo.co.uk>
2020-06-17 20:55:02 +0100

    Added factorisation testing

---
+++ caspy/tests/test_factorisation.py
@@ -0,0 +1,17 @@
+from caspy.factorise import factoriseNum
+from hypothesis import given, settings
+import hypothesis.strategies as st
+
+
+def product(ns: [int]) -> int:
+    if ns == []:
+        return 0
+    val = 1
+    for n in ns:
+        val = val * n
+    return val
+
+
+@given(st.integers(min_value=-100000, max_value=100000))
+def tests_numeric_factorisation(x):
+    assert product(factoriseNum(x)) == x
