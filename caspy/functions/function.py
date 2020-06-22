bd316760db5780c4bcdc68b0222830ffcecd8a39
Tom Gray <tomg77@yahoo.co.uk>
2020-06-22 14:17:37 +0100

    Added equality and hash methods to function so ln(x)+ln(x) -> 2ln(x)

--- caspy/functions/function.py
+++ caspy/functions/function.py
@@ -1,5 +1,10 @@
 class Function:
-    pass
+    def __eq__(self, other):
+        if isinstance(other, Function):
+            return self.fname == other.fname and self.arg == other.arg
+
+    def __hash__(self):
+        return hash("{}({})".format(self.fname,self.arg))
 
 class Function1Arg:
     pass
