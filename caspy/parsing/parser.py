fc9f106e3151485450a59b074803f402264def16
Tom Gray <tomg77@yahoo.co.uk>
2020-06-30 11:34:01 +0100

    Removed old unused classes

--- caspy/parsing/parser.py
+++ caspy/parsing/parser.py
@@ -2,8 +2,6 @@ import os
 import inspect
 import logging
 from lark import Lark
-from caspy.parsing import evaluation
-from caspy.printing.ascii_print import ASCIIPrint
 from caspy.parsing.simplify_output import SimplifyOutput
 
 logger = logging.getLogger(__name__)
@@ -12,10 +10,7 @@ logger = logging.getLogger(__name__)
 class Parser:
     def __init__(self, output="ASCII"):
         self.parser = Lark.open(os.path.join(os.path.dirname(inspect.stack()[0][1]), "grammar.lark"))
-        self.eval = evaluation.EvalLine()
         self.simplifier_transformer = SimplifyOutput()
-        if output == "ASCII":
-            self.outputTrans = ASCIIPrint()
 
 
     def parse(self, line: str):
@@ -23,4 +18,3 @@ class Parser:
         logger.debug("Tree = \n{}\n--".format(tree.pretty()))
         simped = self.simplifier_transformer.transform(tree)
         return simped
-        return self.outputTrans.transform(simped)
