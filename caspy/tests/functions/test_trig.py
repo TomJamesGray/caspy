93db5cb43e9d89c19eec66758620124a85f85e67
Tom Gray <tomg77@yahoo.co.uk>
2020-07-01 12:02:47 +0100

    Adjusted deadline for travis and added sin_odd test

--- caspy/tests/functions/test_trig.py
+++ caspy/tests/functions/test_trig.py
@@ -1,6 +1,7 @@
-from caspy.tests.test_symbolic import p,latex_eval
+from datetime import timedelta
+from caspy.tests.test_symbolic import p, latex_eval
 from caspy.numeric.fraction import Fraction
-from hypothesis import given
+from hypothesis import given, settings
 import hypothesis.strategies as st
 
 sin_pi_coeffs = [
@@ -28,35 +29,51 @@ cos_pi_coeffs = [
 ]
 
 
+# Default deadline is reached on travis, so increase it
+@settings(deadline=timedelta(milliseconds=500))
 @given(st.integers(min_value=-1000, max_value=1000))
 def test_sin_pis(x):
+    """Test sin(n*pi) = 0 for all integers"""
     assert latex_eval("sin({}*pi)".format(x)) == "0"
 
 
+@settings(deadline=timedelta(milliseconds=500))
 @given(st.integers(min_value=-1000, max_value=1000))
 def test_cos_pis(x):
+    """Test cos(n*pi) = 1 for even n and -1 for odd n"""
     if x % 2 == 0:
         assert latex_eval("cos({}*pi)".format(x)) == "1"
     else:
         assert latex_eval("cos({}*pi)".format(x)) == "-1"
 
 
-@given(st.sampled_from(sin_pi_coeffs),st.integers(min_value=-1000,max_value=1000))
-def test_sin_periodicity(pair,period):
+@settings(deadline=timedelta(milliseconds=500))
+@given(st.sampled_from(sin_pi_coeffs), st.integers(min_value=-1000, max_value=1000))
+def test_sin_periodicity(pair, period):
     """Test sin has period 2pi"""
-    assert latex_eval("sin({}*pi + {}*pi)".format(pair[0],2*period)) ==\
-        latex_eval(pair[1])
+    assert latex_eval("sin({}*pi + {}*pi)".format(pair[0], 2 * period)) == \
+           latex_eval(pair[1])
 
 
-@given(st.sampled_from(cos_pi_coeffs),st.integers(min_value=-1000,max_value=1000))
-def test_cos_periodicity(pair,period):
+@settings(deadline=timedelta(milliseconds=500))
+@given(st.sampled_from(cos_pi_coeffs), st.integers(min_value=-1000, max_value=1000))
+def test_cos_periodicity(pair, period):
     """Test cos has period 2pi"""
-    assert latex_eval("cos({}*pi + {}*pi)".format(pair[0],2*period)) ==\
-        latex_eval(pair[1])
+    assert latex_eval("cos({}*pi + {}*pi)".format(pair[0], 2 * period)) == \
+           latex_eval(pair[1])
 
 
-@given(st.sampled_from(cos_pi_coeffs),st.integers(min_value=-1000,max_value=1000))
-def test_cos_even(pair,period):
+@settings(deadline=timedelta(milliseconds=500))
+@given(st.sampled_from(cos_pi_coeffs), st.integers(min_value=-1000, max_value=1000))
+def test_cos_even(pair, period):
     """Test cos is an even function"""
     assert latex_eval("cos({}*pi + {}*pi)".format(pair[0], 2 * period)) == \
            latex_eval("cos(-({}*pi + {}*pi))".format(pair[0], 2 * period))
+
+
+@settings(deadline=timedelta(milliseconds=500))
+@given(st.sampled_from(sin_pi_coeffs), st.integers(min_value=-1000, max_value=1000))
+def test_sin_odd(pair, period):
+    """Test cos is an even function"""
+    assert latex_eval("sin({}*pi + {}*pi)".format(pair[0], 2 * period)) == \
+           latex_eval("-sin(-({}*pi + {}*pi))".format(pair[0], 2 * period))
