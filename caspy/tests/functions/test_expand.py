3d3e5589dbb29aca08c1d08f5c75e128a0d34684
Tom Gray <tomg77@yahoo.co.uk>
2020-07-06 12:12:25 +0100

    Moving away from using latex strings for tests

--- caspy/tests/functions/test_expand.py
+++ caspy/tests/functions/test_expand.py
@@ -1,5 +1,13 @@
-from caspy.tests.test_symbolic import latex_eval
+from caspy.tests.test_symbolic import latex_eval,p
 
 
 def test_square_expansion():
-    assert latex_eval("expand((x+1)^2)") == "x^{2}+2\\cdotx+1"
+    assert p.parse("expand((x+1)^2)") == p.parse("1+x^2+2*x")
+
+
+def test_brackets_expansion():
+    assert p.parse("expand((y+2)*(y+3))") == p.parse("y^2+5*y+6")
+
+
+def test_brackets_and_term_expansion():
+    assert p.parse("expand(x*(x+2+y))") == p.parse("x^2+2*x+y*x")
