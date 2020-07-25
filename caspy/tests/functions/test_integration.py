e2cc2f6570622cab479e223c59852900360d912e
Tom Gray <tomg77@yahoo.co.uk>
2020-07-25 11:23:22 +0100

    Added some tests for integration

---
+++ caspy/tests/functions/test_integration.py
@@ -0,0 +1,17 @@
+import hypothesis.strategies as st
+from caspy.tests.test_symbolic import p
+from hypothesis import given, settings
+
+
+def test_integrate_polyn():
+    assert p.parse("integrate(2*x^2+x+2)") == p.parse("2*x^3/3+x^2/2+2*x")
+
+
+@given(st.floats(min_value=-1000,max_value=1000))
+def test_integrate_const(x):
+    assert p.parse("integrate({})".format(x)) == p.parse("{}*x".format(x))
+
+
+@given(st.floats(min_value=-1000,max_value=1000))
+def test_integrate_const_wrt_y(x):
+    assert p.parse("integrate({},y)".format(x)) == p.parse("{}*y".format(x))
