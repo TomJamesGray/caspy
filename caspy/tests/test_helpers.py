7f93d585d281b307898d1bb6471ffbc097bebfcf
Tom Gray <tomg77@yahoo.co.uk>
2020-08-22 17:07:58 +0100

    Stop failing test

--- caspy/tests/test_helpers.py
+++ caspy/tests/test_helpers.py
@@ -5,5 +5,4 @@ def test_groupings():
     tot = []
     for z in group_list_into_all_poss_pairs([1,2,3]):
         tot.append(z)
-
-    assert tot == 0
+    assert tot == [([1], [2, 3]), ([2], [1, 3]), ([3], [1, 2])]
