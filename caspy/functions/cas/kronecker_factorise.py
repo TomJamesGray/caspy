eb1f256b1b2ff7851944cf161aecfa130c3b471f
Tom Gray <tomg77@yahoo.co.uk>
2020-10-14 13:52:34 +0100

    Got factorisation with Kroneckers method working, just need to output it so caspy understands it

--- caspy/functions/cas/kronecker_factorise.py
+++ caspy/functions/cas/kronecker_factorise.py
@@ -1,4 +1,6 @@
 import logging
+import itertools
+import numpy as np
 import caspy.pattern_match
 from caspy.functions.function import Function1Arg
 from caspy.helpers.helpers import lcm,gcd_l,get_divisors
@@ -17,6 +19,14 @@ def polyn_eval(polyn,x):
     return val
 
 
+def np_polyn_to(polyn):
+    new_repr = []
+    max_pow = len(polyn)-1
+    for index,coeff in enumerate(polyn):
+        if coeff != 0:
+            new_repr.append([Fraction(int(coeff),1),max_pow-index])
+    return new_repr
+
 def kronecker(polyn):
     """
     Factorises a single variable polynomial with Kroneckers method.
@@ -24,6 +34,11 @@ def kronecker(polyn):
     represented by [[a_0,0], [a_1,1], ...]
     :return: Multiple lists representing the polynomial factors
     """
+    n = max(*polyn,key=lambda x: x[1])[1]
+    if n <= 1:
+        # Don't try and factor linear term
+        return [polyn]
+
     denominators = []
     for coeff,_ in polyn:
         denominators.append(coeff.den)
@@ -41,7 +56,6 @@ def kronecker(polyn):
     logger.debug("cont : {}".format(cont))
     logger.debug("v : {}".format(v_polyn))
     # Now have a polynomial in Z[x]
-    n = max(*v_polyn,key=lambda x: x[1])[1]
     logger.debug("n: {}".format(n))
     s = int(n/2)
 
@@ -55,6 +69,7 @@ def kronecker(polyn):
 
     logger.debug("x_i : {}".format(non_zeroes))
 
+    # Calucluate the divisors of the f(non_zeroes)
     f_factors = []
     for _,f_val in non_zeroes:
         divisors_abs = list(get_divisors(abs(f_val)))
@@ -63,8 +78,35 @@ def kronecker(polyn):
         )
     logger.debug("S_i: {}".format(f_factors))
 
-    pass
+    # TEMP while we use np.polydiv get a representation of v_polyn in the
+    # format numpy likes
+    # TODO implement own polynomial division algorithm
+    coeffs_v_np = np.zeros(n+1)
+    for coeff,power in v_polyn:
+        coeffs_v_np[n-power] = coeff
 
+    # Generate matrix with the chosen x values to solve the linear equations
+    mat_rows = []
+    for x_val, func_val in non_zeroes:
+        mat_rows.append([
+            x_val ** power for power in range(s + 1)
+        ])
+    inv_mat = np.linalg.inv(np.array(mat_rows))
+
+    for rhs in itertools.product(*f_factors):
+        q_polyn = np.flipud(inv_mat.dot(np.array(rhs)))
+        quotient,remainder = np.polydiv(coeffs_v_np,q_polyn)
+        if np.array_equal(remainder,np.array([0.])):
+            # We have found divisor
+            print("Found quotient: {}\nq_polyn: {}\nRHS: {}".format(quotient,q_polyn,rhs))
+            # Factor quotient and q_polyn again to see if the answer can be
+            # reduced further
+            quotient_factored = kronecker(np_polyn_to(quotient))
+            q_polyn_factored = kronecker(np_polyn_to(q_polyn))
+            print("Factored: {} {}".format(quotient_factored,q_polyn_factored))
+            return quotient_factored + q_polyn_factored
+    # Couldn't factorise the polynomial
+    return [polyn]
 
 class KroneckerFactor(Function1Arg):
     fname = "factor"
@@ -89,10 +131,10 @@ class KroneckerFactor(Function1Arg):
                 try:
                     c = sym.sym_frac_eval()
                     if c.is_int_frac():
-                        polyn.append([c,Fraction(0,1)])
+                        polyn.append([c,0])
                 except Exception:
                     logger.critical("FAIL")
 
         factored = kronecker(polyn)
-        logger.critical("Repr: {}".format(polyn))
+        logger.critical("Repr: {}".format(factored))
         return super().eval()
