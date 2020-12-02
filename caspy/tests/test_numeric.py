cfbe4462bcd5a74ef13771f8589312c2a5f8c782
Tom Gray <tomg77@yahoo.co.uk>
2020-12-02 13:18:15 +0000

    typo

--- caspy/tests/test_numeric.py
+++ caspy/tests/test_numeric.py
@@ -19,7 +19,7 @@ def test_replace_numeric_with_var_mults():
     assert val.try_replace_numeric_with_var(to_rep,rep_with) == p.parse("U1^(2)")
 
 
-def test_repalce_numeric_with_var_func_arg():
+def test_replace_numeric_with_var_func_arg():
     val = p.parse("x^2+1")
     to_rep = p.parse("x^2+1")
     rep_with = p.parse("U1")
